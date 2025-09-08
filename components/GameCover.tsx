4c1816efad495800bbdc3a2203ff643179003f0b
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-09-07 21:09:50 -0300

    feat: add size prop and styling to GameCover component

--- components/GameCover.tsx
+++ components/GameCover.tsx
@@ -1,10 +1,18 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-function GameCover({ game, className }: { game: Game, className?: string }) {
+type GameCoverSize = 'small' | 'medium' | 'large';
+
+function GameCover({ game, size, className }: { game: Game, size: GameCoverSize, className?: string }) {
   return (
-    <Link href={`/games/${game.id}`} className={cn(className)}>
-      <img src={game.cover} alt={game.title} />
+    <Link href={`/games/${game.id}`} className={
+      cn(
+        size === 'small' ? 'w-16 h-24' : size === 'medium' ? 'w-20 h-32' : 'w-32 h-52',
+        "rounded-[3px] border border-nebula-purple/30",
+        className
+      )
+    }>
+      <img src={game.cover} alt={game.title} className="h-full w-full" />
     </Link>
   )
 
