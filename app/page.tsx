50e3a66d5e8a778189f5ff6c1f729529a42acaac
Caio Dias Marins <91338704+devCaiodias@users.noreply.github.com>
2025-09-09 13:52:20 -0300

    Feature/nav bar (#2)
    * fix: lexend fonts was not working
    
    * chore: Rename logo and add new image
    
    * feat: create GameCover component and Game type
    
    * build: create pnpm-lock.yaml file
    
    * fix: use design system colors
    
    * feat: add size prop and styling to GameCover component
    
    * fix: replace CSS classes with tailwind theme variables
    
    * feat: <NavBar /> component created
    
    * fix: I improved the code and added the Krona One font to use in the navs
    
    * fix: font family variables was in incorrect place
    
    * styles: update nav text size to 12px
    
    ---------
    
    Co-authored-by: Matheus Emanoel <buss.matheusemanoel@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Navbar from "@/components/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
+      <Navbar />
     <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-8 ">
+
       <div className="flex flex-col items-center gap-4 bg-white text-white p-8 rounded-lg">
 
         <Image src="/logoArcadiumWhite.jpg" alt="logo" width={150} height={150} />
@@ -11,5 +15,6 @@ export default function Home() {
         <p className="font-bold w-96 text-center">Frontend do Arcadium: plataforma para avaliação e descoberta de jogos, com rankings, reviews e perfis personalizados.</p>
       </div>
     </div>
+    </>
   );
 }
---
+++ components/NavBar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import logo from "@/public/images/arcadiunslogonav.png"
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="flex items-center justify-center gap-2">
+      <Image src={logo} alt="logo arcad" width={148} height={49} />
+      <nav className="text-white text-sm font-krona font-bold flex space-x-5">
+        <Link href="#">LOGIN</Link>
+        <Link href="#">CRIAR CONTA</Link>
+        <Link href="#">JOGOS</Link>
+        <Link href="#">RANKINGS</Link>
+        <Link href="#">MEMBROS</Link>
+      </nav>
+
+    </header>
+  )
+}
