8ac91b1060c492f372c65f63f7b0235028852c56
devCaiodias <caiodiasmartins12@gmail.com>
2025-09-09 18:25:55 -0300

    fix: Colocando o componente <Search /> No <Navbar />. mudando a imagem para svg e trocando o peso da front

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 import logo from "@/public/images/arcadiunslogonav.png"
 import Link from "next/link";
+import Search from "./Search";
 
 export default function Navbar() {
   return (
     <header className="flex items-center justify-center gap-2">
       <Image src={logo} alt="logo arcad" width={148} height={49} />
-      <nav className="text-white text-sm font-krona font-bold flex space-x-5">
+      <nav className="text-white text-sm font-krona font-normal flex space-x-5">
         <Link href="#">LOGIN</Link>
         <Link href="#">CRIAR CONTA</Link>
         <Link href="#">JOGOS</Link>
         <Link href="#">RANKINGS</Link>
         <Link href="#">MEMBROS</Link>
       </nav>
-
+      <Search />
     </header>
   )
 }
